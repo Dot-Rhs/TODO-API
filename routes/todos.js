b8963f87813d5f3ba6f8c963934aa0d2aeb4f6b8
unknown <rhysh1986@gmail.com>
2019-02-28 16:29:21 +0000

    Add item functionality added

--- routes/todos.js
+++ routes/todos.js
@@ -4,14 +4,35 @@ var connect = require("../connect");
 
 let db;
 
-connect("ToDoList").then(database => {
-  db = database;
-  app.listen(PORT, () => console.log(`I am listening on port ${PORT}`));
+/* GET home page. */
+// router.get("/", function(req, res, next) {
+//   res.render("todos", { title: "To-do List" });
+// });
+
+router.get("/", function(req, res, next) {
+  db.collection("Items")
+    .find({})
+    .toArray(function(err, docs) {
+      if (err) return next(err);
+      // docs.each(function(err, doc) {
+      //   if (doc) {
+      //     console.log(doc);
+      res.json({ "Your list": docs });
+      //   }
+      // });
+    });
 });
 
-/* GET Todo's list. */
-router.get("/", function(req, res, next) {
-  res.render("todos", { title: "To-do List" });
+router.post("/", function(req, rest, next) {
+  var addItem = db.collection("Items");
+  addItem.insert({
+    title: "Get out my car, who are you???", //req.body.title
+    completed: false //req.body.completed
+  });
+});
+
+connect("ToDoList").then(database => {
+  db = database;
 });
 
 module.exports = router;
